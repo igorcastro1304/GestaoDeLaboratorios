38116ec21ba6dd7215cae3901e925385bc68739d
igorcastro1304 <igor.hoffmann@al.infnet.edu.br>
2024-03-18 21:01:46 -0400

    Adicionar arquivos de projeto.

---
+++ GestaoDeLaboratorios/DAL/InfnetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestaoDeLaboratorios.Models;
+
+namespace GestaoDeLaboratorios.DAL
+{
+    public class InfnetDbContext : DbContext
+    {
+        public InfnetDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<GestaoDeLaboratorios.Models.Computador> Computador { get; set; } = default!;
+    }
+}
---
+++ GestaoDeLaboratorios/Models/Computador.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computador
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Processador { get; set; }
+        [Required]
+        [Display(Name = "Placa Mãe")]
+        public string PlacaMae { get; set; }
+        [Required]
+        [Display(Name = "Memória")]
+        public string Memoria { get; set; }
+        [Required]
+        [Display(Name = "HD")]
+        public string Hd { get; set; }
+        [Required]
+        [Display(Name = "Número Patrimonial")]
+        public string NumeroPatrimonio { get; set; }
+        [Required]
+        [Display(Name = "Data da Compra")]
+        public string DataCompra { get; set; }
+    }
+}
